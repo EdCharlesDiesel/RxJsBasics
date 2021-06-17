1ef26eefb44c79f0a72b9c0d4784ed3dbaa1dd75
Khotso.Mokhethi <Khotso.Mokhethi@genasystech.com>
2021-06-17 23:44:01 +0200

    Getting started with Observables.
    We learned how observables are cold by default meaning you would have to subscribe to them for emited values. They are push based and that they can emit one or more values. Observables can be cancelled and that they can deliever values both synchronous and asynchronous.

--- index.ts
+++ index.ts
@@ -1,6 +1,41 @@
-import {Observable,} from 'rxjs';
+import './style.css';
+console.clear();
 
-const observable = new Observable(subscriber=>{
-  subscriber.next('Hello');
+import { Observable } from 'rxjs';
+
+const observer = {
+    next: value => console.log('next', value),
+    error: error => console.log('error', error),
+    complete: () => console.log('complete!')
+};
+
+const observable = new Observable(subscriber => {
+    // subscriber.next('Hello');
+    // subscriber.next('World');
+    // //subscriber.complete();
+
+    // subscriber.next('Hello I said');
+    // subscriber.error('There is an error');
+    // subscriber.next('World');
+
+    let count= 0;
+    const id = setInterval(() => {
+        subscriber.next(count);
+        //subscriber.complete();
+        count += 1;
+    }, 1000);
+
+    return ()=>{
+        console.log('called');
+        clearInterval(id);
+    }
 });
 
+const subscription = observable.subscribe(observer);
+const subscriptionTwo = observable.subscribe(observer);
+
+subscription.add(subscriptionTwo);
+
+setInterval(() =>{
+    subscription.unsubscribe();
+},3500);
